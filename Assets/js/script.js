e7997322d6b5b728f060dc40558c48602b3270f6
Ryan Owens <rtowens11@gmail.com>
2021-03-20 21:37:41 -0500

    initial commit

---
+++ Assets/js/script.js
@@ -0,0 +1,51 @@
+var apiKey = 'e938f1f3684e6e6a9c9fd066694773d2'
+var cityInput = document.getElementById('cityInput')
+var cityButton = document.getElementById('cityButton')
+var currentStatus = document.getElementById('current')
+var selectedCity = ''
+var coordinatesURL = ''
+var cityLat = ''
+var cityLon = ''
+var oneCallUrl = ''
+var currentWeather = {}
+var currentWeatherIcon = ''
+
+
+
+function ingestCityInput(event) {
+    event.preventDefault();
+    console.log(event);
+    console.log(cityInput.value)
+    selectedCity = cityInput.value
+    coordinatesURL = 'https://api.openweathermap.org/geo/1.0/direct?q=' + selectedCity + '&appid=' + apiKey
+
+    fetch(coordinatesURL)
+    .then(function(response){
+    console.log(response);
+    return response.json();
+    })
+    .then(function(data){
+        console.log('success');
+        console.log(data[0]);
+        cityLat = data[0].lat;
+        cityLon = data[0].lon;
+        oneCallUrl = 'https://api.openweathermap.org/data/2.5/onecall?lat=' + cityLat + '&lon=' + cityLon + '&appid=' + apiKey
+
+        fetch(oneCallUrl)
+        .then(function(response){
+            console.log('success2');
+            return response.json();
+        })
+        .then(function(data){
+            console.log(data);
+            console.log(data.current.weather[0])
+            currentWeather = data.current.weather[0]
+            console.log(currentWeather.icon)
+        })
+        currentStatus.textContent = currentWeather.icon
+
+    })
+
+}
+
+cityButton.addEventListener('click', ingestCityInput)
